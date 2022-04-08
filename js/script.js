3242fa34e828ddd60f46e6aba0ca8242cd683f6f
Muhammad Abbas <81623214+AbbasCreations@users.noreply.github.com>
2022-04-09 02:05:05 +0500

    update
    update

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@
 (function ($) {
     "use strict";
       $('.sakura-falling').sakura();
+    $("body").trigger('click');
 })(jQuery);
 
 /**
@@ -20,13 +21,12 @@
  * Listen here: https://youtu.be/X0MDALpV29s
  *
  */
-$("document").ready(function() {
-$("body").trigger('click');
+
+
 $(document).on('click', function(){
     document.getElementById("my_audio").play();
     console.log('Shaadi me zaroor aana');
 });
-    });
 
 // Set the date we're counting down to
 var countDownDate = new Date("Nov 29, 2020 00:00:00").getTime();
